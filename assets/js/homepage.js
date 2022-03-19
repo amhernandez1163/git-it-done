32c81410b956665cc0c97803e35cc4f4c8b7afc2
Amanda Dell <amhernandez1163@gmail.com>
2022-03-18 20:35:11 -0500

    updates to list-issues feature

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -3,28 +3,6 @@ var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
 
-var getUserRepos = function (user) {
-    //format the github api url
-    var apiUrl = "https://api.github.com/users/" + user + "/repos";
-
-    //make a request to the url
-    fetch(apiUrl).then(function(response) {
-        if (response.ok) {
-            response.json().then(function(data) {
-                displayRepos(data, user);
-            });
-        } else {
-            alert("Error: GitHub User Not Found");
-        }
-    })
-    .catch(function(error) {
-        // Notice this `.catch()` getting chained onto the end of the `.then()` method
-        alert("Unable to connect to GitHub");
-    });
-
-};
-
-
 var formSubmitHandler = function(event) {
     event.preventDefault();
 
@@ -37,6 +15,29 @@ var formSubmitHandler = function(event) {
     } else {
         alert("Please enter a GitHub username");
     }
+};
+
+
+var getUserRepos = function (user) {
+    //format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+    //make a request to the url
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                console.log(data);
+                displayRepos(data, user);
+            });
+        } else {
+            alert("Error: GitHub User Not Found");
+        }
+    })
+    .catch(function(error) {
+        // Notice this `.catch()` getting chained onto the end of the `.then()` method
+        alert("Unable to connect to GitHub");
+    });
+
 };
 
 var displayRepos = function(repos, searchTerm) {
@@ -57,8 +58,9 @@ var displayRepos = function(repos, searchTerm) {
         var repoName = repos[i].owner.login + "/" + repos[i].name;
 
         //create a container for each repo
-        var repoEl = document.createElement("div");
+        var repoEl = document.createElement("a");
         repoEl.classList = "list-item flex-row justify-space-between align-center";
+        repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName);
 
         //create a span element to hold repo name
         var titleEl = document.createElement("span");
